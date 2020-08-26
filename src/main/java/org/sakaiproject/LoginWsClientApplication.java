336416e628e36ebd91120addf4209ec348592c1f
David Lara Diaz <david.lara01uptc.edu.co>
2020-08-26 15:13:10 -0500

    creacion de proyecto y index

---
+++ src/main/java/org/sakaiproject/LoginWsClientApplication.java
@@ -0,0 +1,13 @@
+package org.sakaiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWsClientApplication.class, args);
+	}
+
+}
