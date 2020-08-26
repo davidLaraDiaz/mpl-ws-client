605752fae23cf7fa22c03123ba1307010aad2dd9
David Lara Diaz <david.lara01uptc.edu.co>
2020-08-26 15:37:38 -0500

    vista home validada con login

--- src/main/java/org/sakaiproject/controller/ControllerLogin.java
+++ src/main/java/org/sakaiproject/controller/ControllerLogin.java
@@ -1,8 +1,11 @@
 package org.sakaiproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -16,6 +19,12 @@ public class ControllerLogin {
 	@GetMapping(value = "/login")
 	public String login() {
 		return "login";
+	}
+	
+	@PostMapping(value = "/home")
+	public String home(Model model, @RequestParam String id, @RequestParam String pw) {
+		model.addAttribute("res", id+ "   "+pw);
+		return "home";
 	}
 	
 }
